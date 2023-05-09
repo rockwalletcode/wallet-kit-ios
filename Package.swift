4d43ea11552d392a99fb2b5b3aee8bfdce6181e7
Christina Peterson <clp8199@knights.ucf.edu>
2023-05-09 10:34:01 -0400

    Testing default eth gas limit

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(name: "WalletKitCore", url: "https://github.com/rockwalletcode/wallet-kit-core.git", .exact("5.0.20"))
+        .package(name: "WalletKitCore", url: "https://github.com/rockwalletcode/wallet-kit-core.git", .revision("9ea1735a3657cb734f0f4e1fc1fc2479a3af0494"))
     ],
 
     targets: [
