5f4c73183b6bf4325a3563f8f5148d6cc46c3ad7
Christina Peterson <clp8199@knights.ucf.edu>
2023-06-08 16:48:10 -0400

    Updated config file

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(name: "WalletKitCore", url: "https://github.com/rockwalletcode/wallet-kit-core.git", .revision("ffa77d7462a3c77615df464b1d6b1b55b5e80749"))
+        .package(name: "WalletKitCore", url: "https://github.com/rockwalletcode/wallet-kit-core.git", .revision("897b1b542c443957e91a2b65d66a3adc5851192b"))
     ],
 
     targets: [
